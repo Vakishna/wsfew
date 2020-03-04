e0fd11c47052a26d33a35e01aedf835746a71a3d
Vakishna <v-thayalan@outlook.com>
2020-03-05 01:48:34 +1100

    init

---
+++ src/main/java/com/thayalantech/appsamp/AppsampApplication.java
@@ -0,0 +1,13 @@
+package com.thayalantech.appsamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thayalantech/appsamp/AppsampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thayalantech.appsamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppsampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
